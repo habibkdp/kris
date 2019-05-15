c59d5cf0239bcf31db461b7997f50c416b1547e9
habibkdp <habibkdp9@gmail.com>
2019-05-15 23:37:53 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "steviekids@gmail.com";
+$password = "Anelka2000"";
+
+//Pillih Warna 0 untuk hijau 1 untuk kuning
+$collor = "0";
+
+// Pilih Jumlah Spin Saran 1 - 5
+$amount = "1";
+
+
+
